228c9f1bfbb6eb12f1c91a81ba625c0c2a90de09
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-05-23 20:59:34 +0800

    part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,24 @@
  * @jest-environment jsdom
  */
 
+import {pushToHistory} from '../scripts/router.js'
+
+describe("router test", () => {
+    it("settings page", () => {
+        expect(pushToHistory("settings").state).toEqual({page: 'settings'});
+    })
+    it("entry page", () => {
+        let entryNum = 3;
+        expect(pushToHistory("entry", entryNum).state).toEqual({page: `entry${entryNum}`});
+    })
+    it("default page", () => {
+        expect(pushToHistory("wryyyyyyyy").state).toEqual({});
+    })
+    it("stack length", () => {
+        let prev_height = history.length;
+        for (let i = 0; i < 5; i++) {
+            pushToHistory("entry", i);
+        }
+        expect(history.length).toBe(prev_height + 5);
+    })
+})
