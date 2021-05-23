129ac3e2bda8a3fdfe55befff61a89c700f90caf
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-05-24 00:36:23 +0800

    part 2

--- README.md
+++ README.md
@@ -3,11 +3,24 @@
 ## Check your understanding q's (FILL OUT)
 1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
 
+Within a Github action that runs whenever code is pushed & Manually run them locally before pushing code
+
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+I won't because the message feature consists of multiple units, which will make testing each scenario very difficult and complex. Put it in simple word, this feature is too big for a unit test. It belongs to e2e test. 
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
 
-4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+Yes, I would, because the max message length feature is a unit small enough that can be individually tested.
 
-5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+1. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
 
+If headless is set to true, the simulated browser window will not show up.
+
+2. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+
+beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500');
+    await page.waitForTimeout(500);
+    await page.click("header img");
+});
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,12 +29,21 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
-  });
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click("journal-entry")
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/#entry1")
+    })
+  })
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    await page.$("header h1")
+    .then(element => element.getProperty("textContent"))
+    .then(property => property.jsonValue())
+    .then(obj => expect(obj).toEqual("Entry 1"))
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -50,58 +59,204 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
-
+    let content = { 
+      title: 'You like jazz?',
+      date: '4/25/2021',
+      content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+      image: {
+        src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+        alt: 'bee with sunglasses'
+      }
+    };
+    let result = await page.evaluate(() => {
+        let elem = document.querySelector('entry-page');
+        return {
+          title: elem.shadowRoot.querySelector('.entry-title').textContent,
+          date: elem.shadowRoot.querySelector('.entry-date').textContent,
+          content: elem.shadowRoot.querySelector('.entry-content').textContent,
+          image: {
+            src: elem.shadowRoot.querySelector('.entry-image').src,
+            alt: elem.shadowRoot.querySelector('.entry-image').alt
+          }
+        }
+      });
+    expect(result).toEqual(content);
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    let result = await page.evaluate(() => {
+      return document.body.className;
+    })
+    expect(result).toEqual('single-entry');
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click("header img")
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/#settings")
+    })
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    await page.$("header h1")
+    .then(element => element.getProperty("textContent"))
+    .then(property => property.jsonValue())
+    .then(obj => expect(obj).toEqual("Settings"))
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    let result = await page.evaluate(() => {
+      return document.body.className;
+    })
+    expect(result).toEqual('settings');
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await Promise.all([
+      page.waitForNavigation(),
+      page.goBack()
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/#entry1")
+    })
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-
+  it('Test11: Clicking the back button once should bring the user back to the home page', async() => {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.goBack()
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/")
+    })
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
+  it('Test12: When the user if on the homepage, the header title should be “Journal Entries”', async() => {
+    await page.$("header h1")
+    .then(element => element.getProperty("textContent"))
+    .then(property => property.jsonValue())
+    .then(obj => expect(obj).toEqual("Journal Entries"))
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  it('Test13: On the home page the <body> element should not have any class attribute', async() => {
+    let result = await page.evaluate(() => {
+      return document.body.className;
+    })
+    expect(result).toBe("")
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-
+  it('Test14: Verify the url is correct when clicking on the second entry', async() => {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click("journal-entry ~ journal-entry")
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/#entry2")
+    })
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-
+  it('Test15: Verify the title is current when clicking on the second entry', async() => {
+    await page.$("header h1")
+    .then(element => element.getProperty("textContent"))
+    .then(property => property.jsonValue())
+    .then(obj => expect(obj).toEqual("Entry 2"))
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-
+  it('Test16: Verify the entry page contents is correct when clicking on the second entry', async() => {
+    let content = {
+      date: "4/26/2021",
+      title: "Run, Forrest! Run!",
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+          src: "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg",
+          alt: "forrest running"
+      }
+    };
+    let result = await page.evaluate(() => {
+        let elem = document.querySelector('entry-page');
+        return {
+          title: elem.shadowRoot.querySelector('.entry-title').textContent,
+          date: elem.shadowRoot.querySelector('.entry-date').textContent,
+          content: elem.shadowRoot.querySelector('.entry-content').textContent,
+          image: {
+            src: elem.shadowRoot.querySelector('.entry-image').src,
+            alt: elem.shadowRoot.querySelector('.entry-image').alt
+          }
+        }
+      });
+    expect(result).toEqual(content);
+  }, 30000);
 
   // create your own test 17
+  it('Test17: Clicking the back button should bring the user back to the home page', async() => {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.goBack()
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/")
+    })
+  });
 
   // create your own test 18
-
-  // create your own test 19
+  it('Test18: Clicking the forward button, new url should contain /#entry2', async() => {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.goForward()
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/#entry2")
+    })
+  });
 
   // create your own test 20
-  
+  it('Test19: Forwarded to the second Entry page - checking <entry-page> contents ', async() => {
+    let content = { 
+      date: "4/26/2021",
+      title: "Run, Forrest! Run!",
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+          src: "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg",
+          alt: "forrest running"
+      }
+    };
+    let result = await page.evaluate(() => {
+        let elem = document.querySelector('entry-page');
+        return {
+          title: elem.shadowRoot.querySelector('.entry-title').textContent,
+          date: elem.shadowRoot.querySelector('.entry-date').textContent,
+          content: elem.shadowRoot.querySelector('.entry-content').textContent,
+          image: {
+            src: elem.shadowRoot.querySelector('.entry-image').src,
+            alt: elem.shadowRoot.querySelector('.entry-image').alt
+          }
+        }
+      });
+    expect(result).toEqual(content);
+  }, 30000);
+
+  // create your own test 20
+  it('Test20: Clicking on the header should bring the user back to the home page', async() => {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click("header h1")
+    ])
+    .then(() => {
+      expect(page.url()).toEqual("http://127.0.0.1:5500/")
+    })
+  });
 });
